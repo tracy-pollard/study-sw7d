ba3f8ffd54d0429b85a8c21c7d5390295f0074c7
tracy-pollard <118567911+tracy-pollard@users.noreply.github.com>
2023-11-05 17:39:37 +0000

    Created data.compare function in R script: allow comparsion of *2 data frames* even when row and col are not in the same order between the 2 data frames

---
+++ script/fn-data-compare.R
@@ -0,0 +1,38 @@
+# enhance standard dataframe comparison functions
+
+data.compare <- function(df1, df2,...) {
+  
+  library(tidyverse)
+  library(data.table)
+  
+  dt1_ord <- as.data.table(df1) %>% arrange(...)
+  dt2_ord <- as.data.table(df2) %>% arrange(...)
+  
+  return(all.equal(dt1_ord, dt2_ord, ignore.col.order=TRUE))
+  
+}
+
+# sample checks
+
+# first <- data.frame(
+#   Name = c("Jack", "Jack", "Matty", "Sam"),
+#   Age = c(10, 12, 8, 6),
+#   Pocket = c(10.5, 20, 600, 3)
+# )
+# 
+# # expect to fail when compare to first: Sam aged 7 instead of 6
+# second <- data.frame(
+#   Name = c("Sam", "Jack", "Jack", "Matty"),
+#   Age = c(7, 10, 12, 8),
+#   Pocket = c(3, 10.5, 20, 600)
+# )
+# 
+# # expect to pass: even though order of rows and columns are not the same
+# third <- data.frame(
+#   Pocket = c(3, 10.5, 20, 600),
+#   Name = c("Sam", "Jack", "Jack", "Matty"),
+#   Age = c(6, 10, 12, 8)
+# )
+# 
+# check2 <- data.compare(df1 = first, df2 = second, Name, Age)
+# check3 <- data.compare(df1 = first, df2 = third, Name, Age)
